594789d1e3a2e31784d5cf35e2f66a25946085de
ipadorusa <ipadorusa@gmail.com>
2018-08-21 00:31:07 +0900

    first commit2

---
+++ src/components/TodoList.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input type="text" class="todo-input" placeholder="What needs to be done">
+    Todo list goes hear
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'todo-list',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss">
+  .todo-input {
+    width:100%;
+    padding:10px 18px;
+    font-size:18px;
+    margin-bottom: 16px;
+
+    &:focus {
+      outline:0;
+    }
+
+  }
+</style>
